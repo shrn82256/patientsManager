d78f81a8cc6152aba47198e19a8ba842b2182c6f
shrn82256 <shrn82256@gmail.com>
2018-04-29 07:32:48 +0530

    added max date and comments to main.js

--- static/main.js
+++ static/main.js
@@ -1,14 +1,21 @@
 $(document).ready(function() {
+	// to Display eror text on wrong input
 	$(".errorText").each(function(index, el) {
 		$(el).on('click', function(event) {
 			$(this).slideUp();
 		});
 	});
 
+	// To add animation and clickToHide effect to alert
 	$(".alert").each(function(index, el) {
 		$(this).slideDown();
 		$(el).on('click', function(event) {
 			$(this).slideUp();
 		});
+	});
+
+	// so that birthdate is less than today
+	$("#dob").prop('max', function(){
+		return new Date().toJSON().split('T')[0];
 	});
 });
